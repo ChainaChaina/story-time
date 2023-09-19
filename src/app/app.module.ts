6957e86484462ebf83c4a693ecb682d80fd451b6
Lucas Borgesgit <lucasborgesmoraes@gmail.com>
2023-09-19 02:10:06 -0300

    'first'

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { NewHistoryComponent } from './components/pages/new-history/new-history.component';
+import { CharactersComponent } from './components/pages/new-history/components/characters/characters.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    NewHistoryComponent,
+    CharactersComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/pages/new-history/new-history.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-history',
+  templateUrl: './new-history.component.html',
+  styleUrls: ['./new-history.component.scss']
+})
+export class NewHistoryComponent {
+
+}
