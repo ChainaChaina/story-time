bb4a73d9cc8de8763330ab62155275cd6031bae9
Lucas Borgesgit <lucasborgesmoraes@gmail.com>
2023-09-19 17:58:29 -0300

    git init fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { AboutComponent } from './components/pages/about/about.component';
-import { HomeComponent } from './components/pages/home/home.component';
-import { NewHistoryComponent } from './components/pages/new-history/new-history.component';
-import { CharactersComponent } from './components/pages/new-history/components/characters/characters.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    AboutComponent,
-    HomeComponent,
-    NewHistoryComponent,
-    CharactersComponent
+
   ],
   imports: [
     BrowserModule,
